62ef28632a70d38282f40da0f2fe77ec51abf755
Chandhan <Chandhan@192.168.0.90>
2023-01-17 19:27:56 +0530

    Added Bit Converter and LCD_Streamer_Web

---
+++ LCD_Streamer_Web/src/main/java/com/example/demo/LcdStreamerWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcdStreamerWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcdStreamerWebApplication.class, args);
+	}
+
+}
